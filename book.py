63a49b774d84f234bd8c01bde0a47235d2ba353a
DjohnLike <petrovanya92@gmail.com>
2021-04-24 20:13:14 +0300

    Added gitignore file and added defaults var to Book class

--- book.py
+++ book.py
@@ -1,2 +1,7 @@
 class Book:
-    pass
+    __default_title = 'Book Title'
+    __default_author = 'Book Author'
+    __default_description = 'Book Description'
+    __default_year = 'Book create year'
+    __default_genre = ['Book genre']
+    __default_review_list = []
